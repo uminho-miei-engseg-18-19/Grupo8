fec169207f57645ab3f50b4556dd7089a01f3c9f
b-pereira <pereirabruno05@gmail.com>
2019-02-27 23:59:57 +0000

    Adiciona blind e init

--- 03_FICHA/Aula3/blindSignature-app.py
+++ 03_FICHA/Aula3/blindSignature-app.py
@@ -1,24 +1,62 @@
 #!/usr/bin/python
 
 import sys, getopt
+from eVotUM.Cripto import utils
+
+from eVotUM.Cripto import eccblind
+
+
+# blindSignature-app.py -key <chave privada> -bmsg <Blind message>
+
+def showResults(errorCode, blindSignature):
+    print("Output")
+    if (errorCode is None):
+        print("Blind signature: %s" % blindSignature)
+    elif (errorCode == 1):
+        print("Error: it was not possible to retrieve the private key")
+    elif (errorCode == 2):
+        print("Error: init components are invalid")
+    elif (errorCode == 3):
+        print("Error: invalid blind message format")
+
+
 
 def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print 'test.py -i <inputfile> -o <outputfile>'
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print 'test.py -i <inputfile> -o <outputfile>'
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print 'Input file is "', inputfile
-   print 'Output file is "', outputfile
+    eccPrivateKeyPath = ''
+    blindM = ''
+    file = ''
+    d = dict()
+    try:
+        opts, args = getopt.getopt(argv, "h:", ["key=", "bmsg=", "file="])
+    except getopt.GetoptError:
+        print 'blindSignature-app.py -key <chave privada> -bmsg <Blind message>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'blindSignature-app.py --key <chave privada> --bmsg <Blind message>'
+            sys.exit()
+        elif opt in ("--key"):
+            key = arg
+        elif opt in ("--RDash"):
+            blindM = arg
+        elif opt in ("--file"):
+            file = arg
+
+    pemKey = utils.readFile(eccPrivateKeyPath)
+    print("Input")
+    passphrase = raw_input("Passphrase: ")
+    f = open(file, "r")
+    for l in f:
+        w = l.strip().split(":")
+        d[w[0].strip()] = w[1].strip()
+    errorCode, blindSignature = eccblind.generateBlindSignature(pemKey, passphrase, blindM, d["initComponents"])
+    showResults(errorCode, blindSignature)
+
+
+
 
 if __name__ == "__main__":
+    main(sys.argv)
+
+
+
--- 03_FICHA/Aula3/ofusca-app.py
+++ 03_FICHA/Aula3/ofusca-app.py
@@ -1,24 +1,44 @@
-#!/usr/bin/python
-
 import sys, getopt
+from eVotUM.Cripto import eccblind
+
+
+def showResults(errorCode, result):
+    print("Output")
+    if (errorCode is None):
+        blindComponents, pRComponents, blindM = result
+        print("Blind message: %s" % blindM)
+        print("Blind components: %s" % blindComponents)
+        print("pRComponents: %s" % pRComponents)
+    elif (errorCode == 1):
+        print("Error: pRDash components are invalid")
+
 
 def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print 'test.py -i <inputfile> -o <outputfile>'
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print 'test.py -i <inputfile> -o <outputfile>'
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print 'Input file is "', inputfile
-   print 'Output file is "', outputfile
+    pRDashComponents = ''
+    data = ''
+    out_file = ""
+    try:
+        opts, args = getopt.getopt(argv, "h:", ["msg=", "RDash=", "--out"])
+    except getopt.GetoptError:
+        print 'ofusca-app.py --msg <mensagem a assinar> --RDash <pRDashComponents> --out <ficheiro>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'ofusca-app.py --msg <mensagem a assinar> --RDash <pRDashComponents> --out <ficheiro>'
+            sys.exit()
+        elif opt in ("--msg"):
+            data = arg
+        elif opt in ("--RDash"):
+            pRDashComponents = arg
+        elif opt in ("--out"):
+            out_file= arg
+    s = data.encode('utf-8')
+    errorCode, result = eccblind.blindData(pRDashComponents, "0x"+s.hex())
+    with open(out_file, "w") as f:
+        f.write(result + "\n")
+    showResults(errorCode, result)
+
+
 
 if __name__ == "__main__":
+    main(sys.argv)
